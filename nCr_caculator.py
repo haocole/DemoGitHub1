ec8a32be2c6282b592793bf1f099a21a60c32cb6
Creeper Ivanovsky <tmthuan.0907@gmail.com>
2026-08-18 09:58:25 +0700

    feature/nCr-caculation

---
+++ nCr_caculator.py
@@ -0,0 +1,21 @@
+def factorical (number):
+    factorical = 1
+    for i in range(1, number + 1):
+        factorical *= i
+    return factorical
+
+def nCr_caculation(val_n, val_r):
+    if val_r > val_n or val_n < 0:
+        return 0
+    elif val_n == 0 or val_r == 0:
+        return 1
+    return factorical (val_n) / factorical(val_r) * factorical(val_n - val_r)
+
+while True:
+    try:
+        val_1 = int(input("Nhập giá trị thứ nhất: "))
+        val_2 = int(input("Nhập giá trị thức hai: "))
+        print(f"Tổ hợp chập {val_2} của {val_1} phần tử là: {factorical(val_1, val_2)}")
+        note = inpu
+    except ValueError:
+        print("Giá trị phải là một số. Vui lòng thử lại!")
