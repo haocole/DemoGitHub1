eb4671fb45ced0744d29509af5993400e4a5f3ad
Quan <hoquan1360@gmail.com>
2026-08-18 09:17:09 +0700

    them phep nhan 2 so

---
+++ phepnhan2so.py
@@ -0,0 +1,8 @@
+def phep_nhan(a, b):
+    return a * b
+
+so1 = float(input("Nhập số thứ nhất: "))
+so2 = float(input("Nhập số thứ hai: "))
+
+ketqua = phep_nhan(so1, so2)
+print(f"{so1} * {so2} = {ketqua}")
